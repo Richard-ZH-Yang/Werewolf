9276eb25af2fa2bfacd6b93357fcb7106111c574
Richard <richardyangzh@gmail.com>
2022-01-15 19:39:13 -0800

    change the content of app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React from 'react'
 import Signup from './components/Signup'
 import { Container } from 'react-bootstrap'
+import { AuthProvider } from './contexts/AuthContext'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Dashboard from './components/Dashboard'
+// import Login from './components/Login'
+import PrivateRoute from './components/PrivateRoute'
 
 
 function App() {
   return (
     <Container
       className='d-flex align-items-center justify-content-center'
-      style={{ minHeight: '50vh' }}
+      style={{ minHeight: '100vh' }}
     >
       <div className='w-100' style={{ maxWidth: '400px' }}>
-              <Signup />
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <PrivateRoute exact path='/' component={Dashboard} />
 
+              <Route path='/signup' component={Signup} />
+              {/* <Route path='/login' component={Login} /> */}
+            </Switch>
+          </AuthProvider>
+        </Router>
       </div>
     </Container>
   )
