0ef669028154a221b6a5edb9809e66a0bfa91473
Richard <richardyangzh@gmail.com>
2022-01-15 19:43:43 -0800

    search room page start

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Signup from './components/Signup'
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from './contexts/AuthContext'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Dashboard from './components/Dashboard'
+import Dashboard from './components/SearchRoom'
 // import Login from './components/Login'
 import PrivateRoute from './components/PrivateRoute'
 
