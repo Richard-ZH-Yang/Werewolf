2d87e26d96bfdab48018a9a48a357babd90d62fe
Richard <richardyangzh@gmail.com>
2022-02-06 21:53:44 -0800

    able to go back to the main room from erro page

--- src/components/Error.js
+++ src/components/Error.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function Error () {
   return <div>
    <h1>ERROR!</h1>
    <h3>This page does not exist, please try to login</h3>
+   <Link to = '/'> go to home page</Link>
   </div>;
 };
