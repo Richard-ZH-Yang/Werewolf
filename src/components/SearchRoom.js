d4445b1104c5c8a1bca2f72dd4f2c6e9f4ae7365
Richard <richardyangzh@gmail.com>
2022-02-09 15:00:22 -0800

    Search room page basic structure

--- src/components/SearchRoom.js
+++ src/components/SearchRoom.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Card, Button, Alert } from 'react-bootstrap'
+import { Card, Button, Alert, Form } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useNavigate } from 'react-router-dom'
 
@@ -8,30 +8,45 @@ export default function SearchRoom() {
   const { currentUser, logout } = useAuth()
   const navigate = useNavigate()
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("SUBMIT")
+  }
 
   async function handleLogout() {
     setError('')
 
     try {
-        await logout()
-        navigate('/login', { replace: true })
-      } catch {
-        setError('Failed to log out')
-      }
+      await logout()
+      navigate('/login', { replace: true })
+    } catch {
+      setError('Failed to log out')
     }
-    console.log(currentUser)
+  }
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className='text-center mb-4'>Search Room</h2>
-          {error && <Alert variant='danger'>{error}</Alert>}
-          <strong>Email:</strong> {currentUser.email}
-        </Card.Body>
-      </Card>
+      {error && <Alert variant='danger'>{error}</Alert>}
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className='mb-3'>
+          <h1 className='text-center mb-4'>Search Room</h1>
+          <h4 className='text-center mb-4'>
+            {currentUser.email} : {currentUser.displayName}
+          </h4>
+          <Form.Label>Please enter the room number:</Form.Label>
+          <Form.Control type='number' placeholder='i.e.1234' required />
+          <Form.Text className='text-muted'>
+            You could enter an existing room or create a new one
+          </Form.Text>
+        </Form.Group>
+        <Button className='btn text-center w-100 mt-2' type='submit'>
+          Submit
+        </Button>
+      </Form>
 
-      <Button className='btn text-center w-100 mt-2' onClick={handleLogout}>Log out</Button>
+      <Button className='btn text-center w-100 mt-2' onClick={handleLogout}>
+        Log out
+      </Button>
     </>
   )
 }
