8c0d18abd92fd0353aebaa5d219e3eb7df91c41a
Richard <richardyangzh@gmail.com>
2022-02-09 15:32:07 -0800

    search room set up fetching data and set error message if room not found

--- src/components/SearchRoom.js
+++ src/components/SearchRoom.js
@@ -2,15 +2,34 @@ import React, { useState } from 'react'
 import { Card, Button, Alert, Form } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 import { Link, useNavigate } from 'react-router-dom'
+import { useFetch } from './useFetch'
 
 export default function SearchRoom() {
   const [error, setError] = useState('')
+  const [roomNumber, setRoomNumber] = useState(0)
   const { currentUser, logout } = useAuth()
   const navigate = useNavigate()
 
-  const handleSubmit = (e) => {
+  // send a get request, if rejected with 404 status, show an error message. Otherwise direct to the main room page with the correct url
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log("SUBMIT")
+    try {
+      const response = await fetch(`http://localhost:4567/rooms/${roomNumber}`)
+      if (response.status === 404) {
+        setError('cannot find that room')
+        setTimeout(()=> {
+          setError('')
+        }, 2000)
+      }
+      console.log(response.status)
+      const room = await response.json()
+      console.log(room)
+    } catch (e) {
+      console.log(e)
+      console.log('ERROR')
+    }
+
+    // TODO: communicate with the backend, send a get request, if rejected, show an error message, otherwise direct to the main room page with the correct url
   }
 
   async function handleLogout() {
@@ -34,7 +53,12 @@ export default function SearchRoom() {
             {currentUser.email} : {currentUser.displayName}
           </h4>
           <Form.Label>Please enter the room number:</Form.Label>
-          <Form.Control type='number' placeholder='i.e.1234' required />
+          <Form.Control
+            type='number'
+            placeholder='1234'
+            required
+            onChange={(e) => setRoomNumber(e.target.value)}
+          />
           <Form.Text className='text-muted'>
             You could enter an existing room or create a new one
           </Form.Text>
