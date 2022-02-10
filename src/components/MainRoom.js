dbb8971a126c478eb2af7dc33dc2e1c16f8a9648
Richard <richardyangzh@gmail.com>
2022-02-09 16:56:44 -0800

    refactor the mainroom to include the state from custome hooks

--- src/components/MainRoom.js
+++ src/components/MainRoom.js
@@ -1,18 +1,35 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { Link, useNavigate, useParams } from 'react-router-dom'
 import { Card, Button, Alert, Row, Col, Container } from 'react-bootstrap'
 import { useAuth, logout } from '../contexts/AuthContext'
-import { useFetch } from './useFetch'
+// import { useFetch } from './useFetch'
 import Player from './Player'
+import uuid from 'react-uuid'
 
 export default function MainRoom() {
   const { id } = useParams()
-  const { loading, seating, room } = useFetch(`http://localhost:4567/rooms/${id}`)
+  const url = `http://localhost:4567/rooms/${id}`
+
   const [error, setError] = useState('')
   const [currentSeat, setCurrentSeat] = useState(0)
+    const [loading, setLoading] = useState(true)
+    const [room, setRoom] = useState([])
+    const [seating, setSeating] = useState([])
+
   const { currentUser, logout } = useAuth()
   const navigate = useNavigate()
 
+    const getRoom = useCallback(async () => {
+      const response = await fetch(url)
+      const room = await response.json()
+      setRoom(room)
+      setSeating(getSeating(room))
+      setLoading(false)
+    }, [url])
+
+    useEffect(() => {
+      getRoom()
+    }, [url, getRoom])
 
   async function handleLogout() {
     setError('')
@@ -25,23 +42,29 @@ export default function MainRoom() {
     }
   }
 
+
   return (
     <>
-      {/* <CardDeck style={{ display: 'flex', flexDirection: 'row' }}> */}
-      <Container
-        className='h-100'
-        >
+      <Container className='h-100'>
         <Row className='h-100 w-100 align-items-center'>
-            {loading ? (
-              <Col md={20} className='treeViewComponent h-100'>
-                <h1>loading ...</h1>
-              </Col>
-              
-            ) : (
-              seating.map((player, index) => {
-                return <Player key={player.id} {...player} seatNumber={index + 1} loading = {loading} currentSeat = {currentSeat} seating = {seating}/>
-              })
-            )}
+          {loading ? (
+            <Col md={20} className='treeViewComponent h-100'>
+              <h1>loading ...</h1>
+            </Col>
+          ) : (
+            seating.map((player, index) => {
+              return (
+                <Player
+                  key={player.id || uuid()}
+                  {...player}
+                  seatNumber={index + 1}
+                  loading={loading}
+                  currentSeat={currentSeat}
+                  seating={seating}
+                />
+              )
+            })
+          )}
         </Row>
       </Container>
 
@@ -53,3 +76,29 @@ export default function MainRoom() {
 }
 
 
+function getPlayerInfo(room, target) {
+  let name = ''
+  let id = ''
+  room.players.forEach((player) => {
+    if (player.seat === target) {
+      name = player.name
+      id = player.id
+    }
+  })
+  return { name, id }
+}
+
+function getSeating(room) {
+  let seatingPlan = []
+  const currentSeats = []
+  room.players.forEach((player) => {
+    currentSeats.push(player.seat)
+  })
+
+  for (let i = 1; i <= room.maxNumPlayer; i++) {
+    let player = {}
+    player = getPlayerInfo(room, i)
+    seatingPlan.push({ seatNumber: i, name: player.name, id: player.id })
+  }
+  return seatingPlan
+}
