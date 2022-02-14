617b92623fcbf7bc3c3d30fbe0553206b6fc95a3
Richard <richardyangzh@gmail.com>
2022-02-13 18:25:36 -0800

    server.js set up port

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Hello world")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/rooms/:id', (req, res) => {
+ res.json({result: "nice"})
+})
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`)
+}) 
