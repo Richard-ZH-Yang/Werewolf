30bf76beb910f6a2562a3c5376f6744387e28a68
Richard <richardyangzh@gmail.com>
2022-02-13 22:20:46 -0800

    get room with an id from MongoDB

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -5,14 +5,15 @@ const Room = require('../models/roomModel')
 // @desc   get room
 // @route  GET /api/rooms/:id
 // @access Private
-const getRoom = asyncHandler((req, res) => {
-  res.status(200).json({ result: `get room ${req.params.id}` })
+const getRoom = asyncHandler(async (req, res) => {
+  const room = await Room.find({id: req.params.id})
+  res.status(200).json(room)
 })
 
 // @desc   update room
 // @route  PUT /api/rooms/:id
 // @access Private
-const updateRoom = asyncHandler((req, res) => {
+const updateRoom = asyncHandler(async (req, res) => {
    res.status(200).json({ result: `update room ${req.params.id}` })
 
 })
@@ -20,7 +21,7 @@ const updateRoom = asyncHandler((req, res) => {
 // @desc   create room
 // @route  POST /api/rooms
 // @access Private
-const createRoom = asyncHandler((req, res) => {
+const createRoom = asyncHandler(async (req, res) => {
    res.status(200).json({ result: `create a new room` })
 
 })
