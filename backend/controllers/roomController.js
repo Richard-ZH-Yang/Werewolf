b5ed7af566beb1c27806744bdb7cb5fe9021bfb1
Richard <richardyangzh@gmail.com>
2022-02-13 19:41:20 -0800

    add description for rooms api

--- backend/controllers/roomController.js
+++ backend/controllers/roomController.js
@@ -1,10 +1,13 @@
-
-
+// @desc   get room
+// @route  GET /api/rooms/:id
+// @access Private
 const getRooms = (req, res) => {
   res.status(200).json({ result: `get room ${req.params.id}` })
 }
 
-
+// @desc   update room
+// @route  POST /api/rooms/:id
+// @access Private
 const updateRooms = (req, res) => {
    res.status(200).json({ result: `update room ${req.params.id}` })
 
