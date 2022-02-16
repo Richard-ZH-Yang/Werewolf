93bcc1453e54d007d1587d9f6c7f1176a4560a4c
Richard-ZH-Yang <richardyangzh@gmail.com>
2022-02-15 17:57:59 -0800

    fetch players from backend, and sort in decending order of total wins

--- frontend/src/components/LeaderBoard.js
+++ frontend/src/components/LeaderBoard.js
@@ -3,6 +3,30 @@ import { Button, Modal } from 'react-bootstrap'
 
 export default function LeaderBoard({ show, onHide }) {
 
+  const url = `http://localhost:4321/api/players`
+
+  const [players, setPlayers] = useState([])
+  const [loading, setLoading] = useState(true)
+
+   const getLeaderBoard = useCallback(async () => {
+     const response = await fetch(url)
+     const currentPlayers = await response.json()
+
+     // sort the players in descending order of the totalWins
+     currentPlayers.sort((a, b) => {
+       return a.totalWins - b.totalWins
+     })
+
+     currentPlayers.reverse()
+
+     setPlayers(currentPlayers)
+     setLoading(false)
+   }, [url])
+
+     useEffect(() => {
+       getLeaderBoard()
+     }, [getLeaderBoard])
+
   return (
     <Modal show={show} onHide={onHide}>
       <Modal.Header closeButton>
